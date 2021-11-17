e39ddbaa647d071b02b222132df36a3867077b4d
Herve Desrosiers <hdesrosiers@stingray.com>
2021-11-17 07:07:48 -0500

    07-Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 const Home = () => {
-  return ( 
+  const handleClick = (e) => {
+    console.log("Hello, ninjas");
+    console.log(e);
+  };
+  const handleClickAgain = (name, e) => {
+    console.log(`Hello again, ${name}`, e.target);
+  };
+
+  return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("dude", e)}>
+        Click me again
+      </button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;
