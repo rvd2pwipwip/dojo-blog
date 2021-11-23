638c22b7b2b093898ca20eae6025534e2e976616
Herve Desrosiers <hdesrosiers@stingray.com>
2021-11-23 06:05:28 -0500

    19-Handling errors

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false)
-      });
-    }, 1000)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch data for that resource");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
