60ff2bea9c168513ff799e419afde2ffd2dee986
Herve Desrosiers <hdesrosiers@stingray.com>
2021-11-24 08:29:05 -0500

    29-Making a POST request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog)
+
+    setIsPending(true)
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("blog added");
+      setIsPending(false)
+    });
   };
 
   return (
@@ -33,7 +44,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="luigi">luigi</option>
         </select>
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
